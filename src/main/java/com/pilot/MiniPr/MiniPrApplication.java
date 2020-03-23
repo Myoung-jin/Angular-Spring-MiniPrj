cc5a9e4012284b029f21d402e74cc17b095df16b
audwls2736 <audwls6372@gmail.com>
2020-03-23 14:52:35 +0900

    first commit

---
+++ src/main/java/com/pilot/MiniPr/MiniPrApplication.java
@@ -0,0 +1,13 @@
+package com.pilot.MiniPr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniPrApplication.class, args);
+	}
+
+}
